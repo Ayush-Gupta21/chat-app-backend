06c70551699387d04bacdc37d096c2125a7b7aff
Ayush Gupta <ayugupcse@gmail.com>
2022-10-13 00:09:09 +0530

    refactored code

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
+const express = require("express")
+const cors = require("cors")
+const mongoose = require("mongoose")
 
-const app = express();
-require("dotenv").config();
+const app = express()
+require("dotenv").config()
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
+app.use(() => {
+    console.log("middleware called")
+})
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => console.log("MONGODB CONNECTED!"))
-.catch((err) => console.log(err.message));
+.catch((err) => console.log(err.message))
 
-const server = app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, () => {
     console.log(`Server Started on Port: ${process.env.PORT}`)
-});
+})
