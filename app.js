f5c65335c17cf07afe59d040dd2df6aba9d083a5
Ayush Gupta <ayugupcse@gmail.com>
2022-10-18 23:44:00 +0530

    chat messages done

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const cors = require("cors")
 const mongoose = require("mongoose")
 
 const userRoutes = require("./routes/userRoutes")
+const messagesRoute = require("./routes/messagesRoute")
 
 const app = express()
 require("dotenv").config()
@@ -10,8 +11,8 @@ require("dotenv").config()
 app.use(cors())
 app.use(express.json())
 
-
 app.use("/api/auth", userRoutes)
+app.use("/api/messages", messagesRoute)
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
